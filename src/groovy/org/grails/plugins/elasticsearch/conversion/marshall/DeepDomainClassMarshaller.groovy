ef4f7f115fc16f4464b95e9f5bf49108c046179b
Esteban D'Amico <esteban@nan-labs.com>
2014-10-15 17:06:02 -0400

    Transient properties are now considered

--- src/groovy/org/grails/plugins/elasticsearch/conversion/marshall/DeepDomainClassMarshaller.groovy
+++ src/groovy/org/grails/plugins/elasticsearch/conversion/marshall/DeepDomainClassMarshaller.groovy
@@ -3,46 +3,48 @@ package org.grails.plugins.elasticsearch.conversion.marshall
 import org.codehaus.groovy.grails.commons.DomainClassArtefactHandler
 import org.codehaus.groovy.grails.commons.GrailsDomainClass
 import org.codehaus.groovy.grails.commons.GrailsDomainClassProperty
+import org.grails.plugins.elasticsearch.mapping.SearchableClassMapping
+import org.grails.plugins.elasticsearch.mapping.SearchableClassPropertyMapping
 
 class DeepDomainClassMarshaller extends DefaultMarshaller {
     protected doMarshall(instance) {
         def domainClass = getDomainClass(instance)
         // don't use instance class directly, instead unwrap from javaassist
         def marshallResult = [id: instance.id, 'class': domainClass.clazz.name]
-        def scm = elasticSearchContextHolder.getMappingContext(domainClass)
+        SearchableClassMapping scm = elasticSearchContextHolder.getMappingContext(domainClass)
         if (!scm) {
             throw new IllegalStateException("Domain class ${domainClass} is not searchable.")
         }
-        for (GrailsDomainClassProperty prop in domainClass.persistantProperties) {
-            def propertyMapping = scm.getPropertyMapping(prop.name)
+        for (SearchableClassPropertyMapping prop in scm.propertiesMapping) {
+            def propertyMapping = scm.getPropertyMapping(prop.propertyName)
             if (!propertyMapping) {
                 continue
             }
-            def propertyClassName = instance."${prop.name}"?.class?.name
-            def propertyClass = instance."${prop.name}"?.class
-            def propertyValue = instance."${prop.name}"
+            def propertyClassName = instance."${prop.propertyName}"?.class?.name
+            def propertyClass = instance."${prop.propertyName}"?.class
+            def propertyValue = instance."${prop.propertyName}"
 
             // Domain marshalling
             if (DomainClassArtefactHandler.isDomainClass(propertyClass)) {
                 String searchablePropertyName = getSearchablePropertyName()
                 if (propertyValue.class."$searchablePropertyName") {
                     // todo fixme - will throw exception when no searchable field.
-                    marshallingContext.lastParentPropertyName = prop.name
-                    marshallResult += [(prop.name): ([id: propertyValue.ident(), 'class': propertyClassName] + marshallingContext.delegateMarshalling(propertyValue, propertyMapping.maxDepth))]
+                    marshallingContext.lastParentPropertyName = prop.propertyName
+                    marshallResult += [(prop.propertyName): ([id: propertyValue.ident(), 'class': propertyClassName] + marshallingContext.delegateMarshalling(propertyValue, propertyMapping.maxDepth))]
                 } else {
-                    marshallResult += [(prop.name): [id: propertyValue.ident(), 'class': propertyClassName]]
+                    marshallResult += [(prop.propertyName): [id: propertyValue.ident(), 'class': propertyClassName]]
                 }
 
                 // Non-domain marshalling
             } else {
-                marshallingContext.lastParentPropertyName = prop.name
+                marshallingContext.lastParentPropertyName = prop.propertyName
                 def marshalledValue = marshallingContext.delegateMarshalling(propertyValue)
                 // Ugly XContentBuilder bug: it only checks for EXACT class match with java.util.Date
                 // (sometimes it appears to be java.sql.Timestamp for persistent objects)
                 if (marshalledValue instanceof java.util.Date) {
                     marshalledValue = new java.util.Date(marshalledValue.getTime())
                 }
-                marshallResult += [(prop.name): marshalledValue]
+                marshallResult += [(prop.propertyName): marshalledValue]
             }
         }
         marshallResult
